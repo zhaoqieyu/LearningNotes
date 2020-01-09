00a7e9adb72647a6e58e4a3d230bf7e54ef309d0
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-10 00:48:01 +0800

    Create 03_checkPalindrome.py

---
+++ Codesignal/03_checkPalindrome.py
@@ -0,0 +1,35 @@
+"""
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+[Python3] Syntax Tips
+
+# Prints help message to the console
+# Returns a string
+def helloWorld(name):
+    print("This prints to the console when you Run Tests")
+    return "Hello, " + name
+"""
+def checkPalindrome(inputString):
+    return inputString[::-1] == inputString
