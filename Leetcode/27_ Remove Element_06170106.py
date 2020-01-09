426610c06b5a5101de6d4d3e42e0a41d59ee889b
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-10 00:26:14 +0800

    Update 27_ Remove Element_06170106.py

--- Leetcode/27_ Remove Element_06170106.py
+++ Leetcode/27_ Remove Element_06170106.py
@@ -3,7 +3,7 @@ class Solution:
         i= 0
         for j in range(len(nums)): #在num的長度內，所有的值
             if nums[j] != val : #若nums的第某項=目標值
-                nums[i]= nums[j] # 則
+                nums[i]= nums[j] # 則把當前這項的值改成nums[j]
                 i += 1 #i也慢慢遞增
         return (i)
     
