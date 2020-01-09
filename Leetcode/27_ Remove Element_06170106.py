8fecfbec1ca03464088c6ea1ef0479da79104ddd
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-10 00:25:15 +0800

    Update 27_ Remove Element_06170106.py

--- Leetcode/27_ Remove Element_06170106.py
+++ Leetcode/27_ Remove Element_06170106.py
@@ -2,7 +2,7 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i= 0
         for j in range(len(nums)): #在num的長度內，所有的值
-            if nums[j] != val : #若list的第某項=目標值
+            if nums[j] != val : #若nums的第某項=目標值
                 nums[i]= nums[j] # 則
                 i += 1 #i也慢慢遞增
         return (i)
