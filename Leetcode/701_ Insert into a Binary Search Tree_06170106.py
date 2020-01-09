c72606b655eb4908b7f785e54875999c38dcf9e1
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-10 00:01:28 +0800

    Update 701_ Insert into a Binary Search Tree_06170106.py

--- Leetcode/701_ Insert into a Binary Search Tree_06170106.py
+++ Leetcode/701_ Insert into a Binary Search Tree_06170106.py
@@ -8,9 +8,11 @@ class TreeNode:
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
         while not root: ##若該節點不存在
-            return TreeNode(val) 
-        if root.val > val:
-            root.left = self.insertIntoBST(root.left, val)
-        elif root.val < val:
-            root.right = self.insertIntoBST(root.right, val) 
+            return TreeNode(val) #直接回傳
+        if root.val > val: #若樹根的節點大於要找的值
+            root.left = self.insertIntoBST(root.left, val) #則插入左子樹中
+        elif root.val < val: #若樹根的節點小於等於要找的值
+            root.right = self.insertIntoBST(root.right, val) #則插入右子樹中
         return root
+
+#reference : https://blog.csdn.net/fuxuemingzhu/article/details/82385503 
