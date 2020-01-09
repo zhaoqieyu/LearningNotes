f1e990d242e888af0da8bbc639787d6aae3f7950
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-10 00:30:51 +0800

    Update 283_ Move Zeroes_06170106.py

--- Leetcode/283_ Move Zeroes_06170106.py
+++ Leetcode/283_ Move Zeroes_06170106.py
@@ -10,7 +10,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] != 0: #遇到非0整數
                 if current_index != i: #且非index
-                    nums[current_index], nums[i] = nums[i], nums[current_index] #將整數塞到index的位子， 最後陣列長度扣除index就是0的數目，將陣列index之後的數字改為0就完成搬移的動作。
-                current_index += 1
+                    nums[current_index], nums[i] = nums[i], nums[current_index] #將整數塞到index的位子， 最後陣列長度扣除index就是0的數目，將陣列index之後的數字改為0就完成搬移的動作
+                current_index += 1 #current_index遞增，去判斷下一個
             
 #reference : https://skyyen999.gitbooks.io/-leetcode-with-javascript/content/questions/283md.html
