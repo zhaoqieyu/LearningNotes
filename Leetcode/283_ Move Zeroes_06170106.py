fc3be02e7fc9aeb41bb2b8164cabc26480b3cc41
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-09 23:50:01 +0800

    Update 283_ Move Zeroes_06170106.py

--- Leetcode/283_ Move Zeroes_06170106.py
+++ Leetcode/283_ Move Zeroes_06170106.py
@@ -12,3 +12,5 @@ class Solution:
                 if current_index != i:
                     nums[current_index], nums[i] = nums[i], nums[current_index]
                 current_index += 1
+            
+#reference : https://skyyen999.gitbooks.io/-leetcode-with-javascript/content/questions/283md.html
