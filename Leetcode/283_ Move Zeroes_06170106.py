2d589c0fabe6bb908af3e1ca8740ab189718c61d
zhaoqieyu <55725136+zhaoqieyu@users.noreply.github.com>
2020-01-09 23:09:32 +0800

    Create 283_ Move Zeroes_06170106.py

---
+++ Leetcode/283_ Move Zeroes_06170106.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+        
+        current_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if current_index != i:
+                    nums[current_index], nums[i] = nums[i], nums[current_index]
+                current_index += 1
